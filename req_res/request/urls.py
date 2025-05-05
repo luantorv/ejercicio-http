8e0bd650bda513ce35f8c2538872c3d98ba13548
luantorv <149726400+luantorv@users.noreply.github.com>
2025-05-05 18:59:07 -0300

    add ALL paths

--- req_res/request/urls.py
+++ req_res/request/urls.py
@@ -3,5 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('home/', views.index, name="Index")
+    path('home/', views.home, name="home"),
+    path('request/', views.req, name="request"),
+    path('request/app-attributes/', views.app_attributes, name="app-attributes"),
+    path('request/middleware/', views.middleware, name="middleware"),
+    path('request/querydict/', views.querydict, name="querydict"),
+    path('request/is-secure/', views.is_secure, name="is-secure"),
+    path('response/', views.res, name="response"),
+    path('response/subclasses/', views.subclasses, name="subclasses"),
+    path('response/json/', views.json, name="json"),
+    path('response/streaming/', views.streaming, name="straming"),
+    path('response/file/', views.file, name="file"),
+    path('response/base/', views.base, name="base"),
 ]
--- req_res/request/views.py
+++ req_res/request/views.py
@@ -2,5 +2,44 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    # context = base_context()
+    return render(request, 'request/index.html')
+
+def home(request):
+    return render(request, 'request/index.html')
+
+# /request/{algo}
+
+def req(request):
+    return render(request, 'request/index.html')
+
+def app_attributes(request):
+    return render(request, 'request/index.html')
+
+def middleware(request):
+    return render(request, 'request/index.html')
+
+def querydict(request):
+    return render(request, 'request/index.html')
+
+def is_secure(request):
+    return render(request, 'request/index.html')
+
+# /response/{algo}
+
+def res(request):
+    return render(request, 'request/index.html')
+
+def subclasses(request):
+    return render(request, 'request/index.html')
+
+def json(request):
+    return render(request, 'request/index.html')
+
+def streaming(request):
+    return render(request, 'request/index.html')
+
+def file(request):
+    return render(request, 'request/index.html')
+
+def base(request):
     return render(request, 'request/index.html')
